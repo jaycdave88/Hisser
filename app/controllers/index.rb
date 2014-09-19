85bcbda515d13284556b70b63b945a74fab6b269
Jay Dave´ & Ruskin <pair+jaycdave88.jonnyparris@devbootcamp.com>
2014-09-19 12:36:52 -0700

    register routes tests passing

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -26,13 +26,16 @@ end
 
 
 post '/snake_hatch' do
+	if params[:snake_name] == "" || params[:password] == ""
+		redirect '/snake_hatch'
+	else
 	user_find = User.find_by(snake_name: params[:snake_name])
+	end
 	if user_find == nil
 	user = User.create(params)
 		session[:user_id] = user.id
-		redirect '/snake_hatch'
+		redirect '/'
 	end
-	redirect '/'
 end
 
 
