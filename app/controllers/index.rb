1c6b390214e3ca4bf8bed407691121bd451aa8e2
Joe Sabato & alexander <pair+reticulin.sayshay@devbootcamp.com>
2014-09-18 22:46:47 -0700

    welocme page implemented, updated routes, ready to sleep

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,29 +3,35 @@ get '/' do
 end
 # home page
 
-
-get '/snake_hatch' do 
-
+# register page
+get '/snake_hatch' do
 	erb :create
 end
-# register page
 
-get '/:user_id' do
-	user_find = User.find_by(snake_name: params[:snake_name])
-	if user_find == nil
+get '/welcome_page' do
+	erb :welcome
+end
+
+get '/check' do
+	user = User.find_by(snake_name: params[:name])
+
+	if user == nil
 		erb :not_found
 	else
 		session[:user_id] = user.id
+		redirect "/welcome_page"
 		erb :welcome
 	end
 end
-# user's page
+
 
 post '/snake_hatch' do
 	user_find = User.find_by(snake_name: params[:snake_name])
 	if user_find == nil
 	user = User.create(params)
 		session[:user_id] = user.id
-	end 
+	end
 	redirect '/'
 end
+
+
