94749916c98a308ba3d04b0cdf612a2332796d00
Joseph Sabato <Reticulin@Josephs-MacBook-Air.local>
2014-09-19 15:22:37 -0700

    finished authentication

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,29 +1,38 @@
 get '/' do
+	session[:user_id] = nil
 	erb :index
 end
 # home page
 
 # register page
 get '/snake_hatch' do
+	session[:user_id] = nil
 	erb :create
 end
 
 get '/welcome_page' do
+	redirect '/' if session[:user_id] == nil
 	erb :welcome
 end
 
 get '/check' do
-	user = User.find_by(snake_name: params[:name])
+	user = User.find_by(snake_name: params[:snake_name])
+
+	if user.password != params[:password]
+		redirect '/'
+	end
+
 	if user == nil
 		erb :not_found
 	else
-		session[:user_id] = user.id
+		session[:user_id] = user.snake_name
 		redirect "/welcome_page"
 		erb :welcome
 	end
 end
 
 get '/:user_id' do
+	redirect '/' if session[:user_id] == nil
 	erb :snake_profile
 end
 
